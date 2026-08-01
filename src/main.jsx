bdd17d46ea522a58ac63635802b3a477465ae00b
emreginacelicr25-code <emreginacelicr25@gmail.com>
2026-07-31 23:00:14 -0300

    Add ErrorBoundary component for error handling

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,45 @@ import App from "./App.jsx";
 import Painel from "./pages/Painel.jsx";
 import "./index.css";
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { erro: null };
+  }
+  static getDerivedStateFromError(erro) {
+    return { erro };
+  }
+  componentDidCatch(erro, info) {
+    console.error("Erro capturado pelo ErrorBoundary:", erro, info);
+  }
+  render() {
+    if (this.state.erro) {
+      return (
+        <pre
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            right: 0,
+            background: "#7f1d1d",
+            color: "#fff",
+            padding: 16,
+            fontSize: 12,
+            whiteSpace: "pre-wrap",
+            zIndex: 99999,
+            maxHeight: "100vh",
+            overflow: "auto",
+          }}
+        >
+          ERRO NO REACT:{"\n"}
+          {String(this.state.erro && this.state.erro.stack ? this.state.erro.stack : this.state.erro)}
+        </pre>
+      );
+    }
+    return this.props.children;
+  }
+}
+
 function Root() {
   const isPainel = window.location.hash.startsWith("#painel");
   return isPainel ? <Painel /> : <App />;
@@ -11,6 +50,8 @@ function Root() {
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Root />
+    <ErrorBoundary>
+      <Root />
+    </ErrorBoundary>
   </React.StrictMode>
 );
